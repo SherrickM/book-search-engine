2b716eea65e0a9854deeb857e4cb739a3a4c406c
Marquis <smcneal89@gmail.com>
2022-09-09 11:05:58 -0400

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,20 +1,27 @@
 const express = require('express');
-
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
-
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
@@ -27,5 +34,6 @@ const startApolloServer = async (typeDefs, resolvers) => {
     })
   })
   };
-
+  
   startApolloServer(typeDefs, resolvers);
+ 
